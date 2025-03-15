de87541017b9e681c26b9d3cc2bed67e500f188f
Movefish <2705441813@qq.com>
2025-03-16 00:29:47 +0800

    加入一个特效动画

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+from PyQt5.QtCore import QPropertyAnimation, QEasingCurve
 
 class GitLearningApp(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
         self.commit_count = 0
+        self.animation = QPropertyAnimation(self.label, b"windowOpacity")
+        self.animation.setDuration(1000)
+        self.animation.setEasingCurve(QEasingCurve.OutQuad)
 
     def initUI(self):
         # 创建界面元素
@@ -29,6 +33,10 @@ class GitLearningApp(QWidget):
     def handle_commit(self):
         self.commit_count += 1
         self.label.setText(f'成功完成 {self.commit_count} 次commit!\n现在尝试在终端运行：\ngit commit -m "第{self.commit_count}次提交"')
+        # 启动淡入动画
+        self.animation.setStartValue(0)
+        self.animation.setEndValue(1)
+        self.animation.start()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
